1d213f61842c874d22c58b6b94caf2e16a89bd8e
Sayan20012002 <sayan20012002@gmail.com>
2021-10-08 18:44:59 +0530

    https://github.com/Sayan20012002/programme.git

---
+++ C Prog/Star and Pyramid series/Left_sided_star.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int row, i, j;
+    printf("\nHow many rows you wanted to print : ");
+    scanf("%d", &row);
+    i = 1;
+
+    while (i <= row)
+    {
+        j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
